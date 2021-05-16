2e9977d72bbfe86da6f8285e2cc460c32f97dead
ssonghyun101 <songhyun0868@gmail.com>
2021-05-16 20:38:32 +0900

    xml, binding 이름 변경

--- app/src/main/java/com/example/firstseminar/SignInActivity.kt
+++ app/src/main/java/com/example/firstseminar/SignInActivity.kt
@@ -7,8 +7,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import com.example.firstseminar.api.ServiceCreater
-import com.example.firstseminar.databinding.ActivityMain2Binding
-import com.example.firstseminar.databinding.ActivityMainBinding
+import com.example.firstseminar.databinding.ActivitySigninBinding
 import com.example.firstseminar.request.RequsetLoginData
 import com.example.firstseminar.response.ResponseLoginData
 import retrofit2.Call
@@ -16,7 +15,7 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class SignInActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivitySigninBinding
     //바인딩을 쓰기 위해 그레이들스크립트 - 빌드그레이들모듈 뭐시기에 가서 뭐써주는거 있는데 알아서보고왹
     //바인딩 클래스를 모든 범위에서 쓰기 위해서 젤 상단에 써준다!!!
     //커멜 표기법인 ActivityView 으로 바인딩 객체를 만든당!
@@ -29,7 +28,7 @@ class SignInActivity : AppCompatActivity() {
         Log.d("첫번째화면","oncreate()")
 
         //뷰가 생성되는 순간에 바인딩 객체 생성 및 호출함수(inflate)!
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivitySigninBinding.inflate(layoutInflater)
         setContentView(binding.root)
         // 사용할 뷰에 대한 참조. .root로 get호출
         //setContentView로 xml받아오기!
