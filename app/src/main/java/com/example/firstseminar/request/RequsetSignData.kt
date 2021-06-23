50f65626e70b153b4fd1b082a85d6bad65d5448a
ssonghyun101 <songhyun0868@gmail.com>
2021-06-23 17:19:20 +0900

    회원가입 sex 삭제

--- app/src/main/java/com/example/firstseminar/request/RequsetSignData.kt
+++ app/src/main/java/com/example/firstseminar/request/RequsetSignData.kt
@@ -3,7 +3,6 @@ package com.example.firstseminar.request
 data class RequsetSignData(
         val email: String,
         val password : String,
-        val sex :String,
         val nickname : String,
         val phone : String,
         val birth : String
